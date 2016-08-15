85d95df417c4f6449113c778c1d4b2c87d1dec16
idk <idk@devuan.local>
2016-08-15 18:12:06 -0400

    trying to build the userspace driver for my tablet

--- Makefile
+++ Makefile
@@ -1,3 +1,8 @@
+
+ARCH=arm
+CROSS_COMPILE=arm-linux-gnueabihf-
+PREFIX=/bin
+
 gslx680: driver.o
 	gcc -o gslx680 driver.o -lm
 
@@ -5,10 +10,10 @@ driver.o: driver.c driver.h
 	gcc -c -o driver.o driver.c
 
 install:
-	install gslx680 /bin
-	#install igslx680.init /etc/init.d/igslx680
-	install gslx680.service /etc/systemd/system/gslx680.service
-	#rm -f /etc/init.d/gslx680
+	install gslx680 $(PREFIX)
+	install igslx680.init /etc/init.d/igslx680
+	#install gslx680.service /etc/systemd/system/gslx680.service
+	rm -f /etc/init.d/gslx680
 	#update-rc.d igslx680 defaults
 
 clean:
---
+++ debian.sh
@@ -0,0 +1,53 @@
+#! /bin/sh
+# Configure your paths and filenames
+SOURCEBINPATH=.
+SOURCEBIN=gsl1680
+SOURCEDOC=README.md
+DEBFOLDER=gsl1680
+DEBVERSION=$(date +%Y%m%d)
+CONTROL_FILE="Source: gsl1680
+Section: admin
+Priority: optional
+Maintainer: Sergio Costas Raster Software Vigo <raster@rastersoft.com>
+Build-Depends: debhelper (>= 9)
+Standards-Version: 3.9.5
+Homepage: https://github.com/rastersoft/gsl1680
+Vcs-Git: git@github.com:rastersoft/gsl1680
+Vcs-Browser: https://github.com/rastersoft/gsl1680
+
+Package: gsl1680
+Architecture: all
+Depends: \${misc:Depends}
+Description: gsl1680 is a user-space driver for Silead gslx68x touch screen
+ devices. It requires device firmware to function.
+ .
+ "
+DEBFOLDERNAME="../$DEBFOLDER-$DEBVERSION"
+
+cd $DEBFOLDER
+
+# Create your scripts source dir
+
+# Copy your script to the source dir
+cp $SOURCEBINPATH/ $DEBFOLDERNAME -r
+cd $DEBFOLDERNAME
+
+# Create the packaging skeleton (debian/*)
+dh_make --indep --createorig
+echo "$CONTROL_FILE" > debian/control
+
+# Remove make calls
+#grep -v makefile debian/rules > debian/rules.new
+#mv debian/rules.new debian/rules
+
+# debian/install must contain the list of scripts to install
+# as well as the target directory
+#echo $DEBFOLDER usr/bin > debian/install
+#echo $SOURCEDOC usr/share/doc/apt-git >> debian/install
+
+# Remove the example files
+rm debian/*.ex
+
+# Build the package.
+# You  will get a lot of warnings and ../somescripts_0.1-1_i386.deb
+debuild -us -uc > ../log
